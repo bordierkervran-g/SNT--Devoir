56ada57adf7ccc398aa131285b690029c39f91be
bordierkervran-g <122128449+bordierkervran-g@users.noreply.github.com>
2023-01-29 19:40:19 +0100

    Add files via upload

---
+++ SNT/javascript.js
@@ -0,0 +1,38 @@
+window.onload = () => {
+    state = true;
+    mainLink = document.querySelector("#MainLink");
+    presentLink = document.querySelector("#PresentLink");
+    mainPage = document.getElementById("me");
+    presentPage = document.getElementById("present");
+    mainLink.addEventListener("click", () => {
+        state = true;
+        actualise();
+    });
+    presentLink.addEventListener("click", () => {
+        state = false;
+        actualise();
+    });
+    actualise();
+    function actualise() {
+        mainOff = document.querySelector("#MainLink").offsetWidth;
+        presentOff = document.querySelector("#PresentLink").offsetWidth;
+        if (state) {
+            document.body.style.setProperty("--W", mainOff + 30 + "px");
+            document.body.style.setProperty("--M", 48 + 15 + "px");
+            document.querySelector("#MainLink").style.color = "#000";
+            document.querySelector("#PresentLink").style.color = "#fff";
+            presentPage.style.marginLeft = 0;
+            mainPage.style.opacity = 1;
+        } else {
+            document.body.style.setProperty("--W", presentOff + 30 + "px");
+            document.body.style.setProperty(
+                "--M",
+                "calc(" + (mainOff + 43) + "px + 48px)"
+                );
+            document.querySelector("#PresentLink").style.color = "#000";
+            document.querySelector("#MainLink").style.color = "#fff";
+            presentPage.style.marginLeft = "-100vw"
+            mainPage.style.opacity = 0;
+        }
+    }
+};
